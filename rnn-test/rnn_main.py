38245101c2f7400789309b0e0184e3d58dd5b6a3
zhuoyuan <chenzhuoyuan07@gmail.com>
2017-03-05 22:44:30 -0800

    rnn wrapper study

--- rnn-test/rnn_main.py
+++ rnn-test/rnn_main.py
@@ -4,7 +4,7 @@ import numpy as np
 import glog as log
 
 # First, we need various cells as basic building blocks
-cell_size = 10
+cell_size = 2
 cell_lstm = tf.contrib.rnn.BasicLSTMCell(cell_size)
 cell_rnn = tf.contrib.rnn.BasicRNNCell(cell_size)
 
@@ -15,30 +15,34 @@ input_np_ = np.array([range(0,num_step), range(1,num_step+1), range(2,num_step+2
 input_np_ = input_np_.reshape([batch_size, num_step, 1]).astype(np.float32)
 print input_np_.shape
 input_ = tf.placeholder(tf.float32, shape=[batch_size, num_step,1])
+
+# input_[batch_size, T, cell_size] to [T, batch_size, cell_size] first
+# then decompose into length=T list, each of shape [batch_size, cell_size] 
+input_unpack = tf.unstack(tf.transpose(input_, perm=[1,0,2]))
 init_state = cell_lstm.zero_state(batch_size=batch_size, dtype=tf.float32)
-state = init_state
-outputs = []
-with tf.variable_scope("RNN"):
-  for time_step in range(num_step):
-    if time_step > 0:
-      tf.get_variable_scope().reuse_variables()
-    (cell_output, state) = cell_lstm(input_[:,time_step,:], state)
-    outputs.append(cell_output)
-# Now outputs is a length(10) list
-# each item is an ouput tensor of shape [3, 10], batch_size * cell_size
+
+# outputs_rnn: a list of length = T, each of shape [batch_size, cell_size]
+# outputs_rnn will have all 0s for unvalid sequence
+outputs_rnn, state = tf.contrib.rnn.static_rnn(cell_lstm, input_unpack, initial_state=init_state, 
+                               sequence_length=[3,4,3])
+
+# loss should be computed with mask like: loss * mask_flag
+# or with tf.contrib.legacy_seq2seq.sequence_loss_by_example([logits], [target], [weight])
 
 # rearrange output to compute loss with ground-truth labels
-# first concatenate to size (3, 100) to maintain batch_size, then resize to (30, 10)
-output = tf.reshape(tf.concat(outputs,1), [-1, cell_size])
+# first concatenate to size (batch_size, T*cell_size) to maintain batch_size, then resize to (batch_size*T, cell_size)
+output = tf.reshape(tf.concat(outputs_rnn,1), [-1, cell_size])
+log.info('output size:')
+print output.get_shape()
 
 # See the graph
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-# summary_writer = tf.summary.FileWriter('.', sess.graph)
+# summary_writer = tf.summary.FileWriter('.', sess.graph)'''
 
-outputs_np = sess.run(outputs, feed_dict={input_: input_np_})
-# print outputs_np
-print outputs_np[0].shape
+outputs_np = sess.run(output, feed_dict={input_: input_np_})
+print outputs_np
+# print outputs_np[0].shape
 
 # Following lines will print nothing if only cell is declared,
 # since it is not created during declaration
