c3da659d56d6ebe8fd12cb70ba1490bc15e6c4f1
zhuoyuan <chenzhuoyuan07@gmail.com>
2017-03-14 15:30:37 -0700

    check in final version working

--- optimizer_test/l2l-final.py
+++ optimizer_test/l2l-final.py
@@ -4,7 +4,7 @@ import glog as log
 
 
 # problem
-batch_size = 10000 # 10000  # 100000
+batch_size = 10000  # 100000
 dim = 3
 sgd_step = 1
 sample_step = 500000  # 1000
@@ -15,13 +15,14 @@ lr_slow = 1e-3
 
 sample_x_flag = True
 sample_w_true_flag = True
-old_update_flag = False
+old_update_flag = False  # to enable back-prop for grad(Wf) to update Ws
 print_debug_flag = False
 
+# set a random seed to make the experiment reproducible
 tf.set_random_seed(1)
 np.random.seed(123)
 
-# np_transform = np.array([[1., 2., 2.], [0., 1., 2.], [0., 0., 1.]]).astype(np.float32)
+# generate off-diagonal elements, sample from a N(0, 1)
 np_random = np.random.normal(size=[dim*(dim-1)/2])
 np_transform = np.identity(dim)
 count = 0
@@ -30,14 +31,13 @@ for i in range(dim):
     np_transform[i,j] = np_random[count]
     count += 1
 np_transform_inv = np.linalg.inv(np_transform)
+transform = tf.constant(np_transform, dtype=tf.float32)
     
-# Ws = tf.get_variable('Ws', dtype=tf.float32, initializer = np_transform_inv.astype(np.float32), trainable=True)
-# (pow(1./lr_fast, 0.3) * 
+# Slow learner initialization, starting from eye(dim)
 Ws = tf.get_variable('Ws', dtype=tf.float32, initializer = np.identity(dim).astype(np.float32), trainable=True)
 
-transform = tf.constant(np_transform, dtype=tf.float32)
+# Fast learner, re-init to zero for each new task
 Wf = tf.get_variable('Wf', shape=[1, dim], dtype=tf.float32, initializer=tf.constant_initializer(0.), trainable=True) 
-
 reset_fast_op = tf.variables_initializer([Wf])
 
 if sample_w_true_flag:
@@ -46,16 +46,15 @@ else:
   W_true = tf.get_variable('W_true', shape=[1, dim], dtype=tf.float32, trainable=False)
     
 if sample_x_flag:
-  # log.info('sampled x')
-  x = tf.placeholder(tf.float32, shape=[dim, batch_size])
+  x1 = tf.placeholder(tf.float32, shape=[dim, batch_size])
   x2 = tf.placeholder(tf.float32, shape=[dim, batch_size])
 else:
-  x = tf.get_variable('x', shape=[dim, batch_size], dtype=tf.float32, trainable=False)
+  x1 = tf.get_variable('x1', shape=[dim, batch_size], dtype=tf.float32, trainable=False)
   x2 = tf.get_variable('x2', shape=[dim, batch_size], dtype=tf.float32, trainable=False)
-  # x = tf.random_normal([batch_size, dim], 0., 1., dtype = tf.float32)
 
+# Step 1: fast learner update from x1
 with tf.name_scope('graph1'):
-  x_transform1 = tf.matmul(transform, x)
+  x_transform1 = tf.matmul(transform, x1)
     
   # y_eps = tf.random_normal([batch_size, 1], 0., 0.0001, dtype = tf.float32)
   with tf.name_scope('ground_truth'):
@@ -63,17 +62,17 @@ with tf.name_scope('graph1'):
 
   with tf.name_scope('predict'):
     y_pred = tf.matmul(Wf, tf.matmul(Ws, x_transform1))
-  # W_pred = tf.matmul(Wf, Ws)
   with tf.name_scope('loss_fast'):
     loss = 0.5 * tf.reduce_mean(tf.square(y_pred - y_true))
 
+# Wf update
 grads_Wf = tf.gradients(loss, [Wf])
 grads_Wf = grads_Wf[0]
 
+# Step 2: slow learner update from x2
 with tf.name_scope('graph2'):
   Wf_new = - lr_fast * grads_Wf
   # Wf_new = lr_fast * tf.transpose(tf.matmul(tf.matmul(Ws, x_transform1), tf.transpose(y_true)))
-  # print 'Wf_new shape: ', Wf_new.get_shape()
   with tf.name_scope('loss_fast_update'):
     loss_old_update = 0.5 * tf.reduce_mean(tf.square(tf.matmul(Wf_new,tf.matmul(Ws,x_transform1)) - y_true))
 
@@ -84,10 +83,6 @@ with tf.name_scope('graph2'):
     y_pred2 = tf.matmul(Wf_new, tf.matmul(Ws, x_transform2))
   with tf.name_scope('loss_slow'):
     loss_new = 0.5 * tf.reduce_mean(tf.square(y_pred2 - y_true2))
-  # grads_Ws = tf.gradients(loss_new, [Ws])
-
-for var in tf.trainable_variables():
-  print var.name
 
 # optimizer class
 if old_update_flag:
@@ -116,17 +111,12 @@ else:
   # Phase 2: Apply gradient
   slow_op = optimizer_s.apply_gradients(zip([grad_slow], [Ws]))
 
-# log.info('Trainable Variables')
-# for var in tf.trainable_variables():
-#   log.info(var.name)
-
 ### train
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 summary_writer = tf.summary.FileWriter('.', sess.graph)
 
 print 'true whitening:', np_transform_inv
-# print 'true param:', sess.run(W_true)
 
 True_sigma = np.matmul(np_transform, np.transpose(np_transform))
 True_sigma_inv = np.linalg.inv(True_sigma)
@@ -143,67 +133,49 @@ for sample_w in range(sample_step):
   for i in range(sgd_step):
     if old_update_flag:
       # fast learner update for one step
-      old_loss = sess.run(loss, feed_dict={x:xnp1, W_true:W_true_np}) #, feed_dict={y_eps: y_eps_np})
+      old_loss = sess.run(loss, feed_dict={x1:xnp1, W_true:W_true_np}) #, feed_dict={y_eps: y_eps_np})
       # fast op
       sess.run(fast_op, feed_dict={x: xnp1, W_true:W_true_np})
-      new_loss = sess.run(loss, feed_dict={x:xnp1, W_true:W_true_np}) #, feed_dict={y_eps: y_eps_np})
+      new_loss = sess.run(loss, feed_dict={x1:xnp1, W_true:W_true_np}) #, feed_dict={y_eps: y_eps_np})
 
       # slow learner update
-      old_loss_slow = sess.run(loss, feed_dict={x:xnp2, W_true:W_true_np})
+      old_loss_slow = sess.run(loss, feed_dict={x1:xnp2, W_true:W_true_np})
       sess.run(slow_op, feed_dict={x:xnp2, W_true:W_true_np})
-      new_loss_slow = sess.run(loss, feed_dict={x:xnp2, W_true:W_true_np})
+      new_loss_slow = sess.run(loss, feed_dict={x1:xnp2, W_true:W_true_np})
     else:
-      # if (sample_w+1) % visualize_step == 10:
-      #  print ('step %d' % (sample_w+1))
-      # print sess.run(Wf_new, feed_dict={x:xnp1, W_true:W_true_np})
-      old_loss = sess.run(loss, feed_dict={x:xnp1, W_true:W_true_np}) #, feed_dict={y_eps: y_eps_np})
-      # update Wf_new is contained in the code
-      new_loss = sess.run(loss_old_update, feed_dict={x:xnp1, W_true:W_true_np}) #, feed_dict={y_eps: y_eps_np})
+      # to monitor loss decrease from 1-sgd fast-learner update
+      old_loss = sess.run(loss, feed_dict={x1:xnp1, W_true:W_true_np}) #, feed_dict={y_eps: y_eps_np})
+      new_loss = sess.run(loss_old_update, feed_dict={x1:xnp1, W_true:W_true_np}) #, feed_dict={y_eps: y_eps_np})
       
       # slow learner update
-      old_loss_slow = sess.run(loss_new, feed_dict={x:xnp1, x2:xnp2, W_true:W_true_np})
-      sess.run(slow_op, feed_dict={x: xnp1, x2:xnp2, W_true:W_true_np})
-      new_loss_slow = sess.run(loss_new, feed_dict={x:xnp1, x2:xnp2, W_true:W_true_np})
+      old_loss_slow = sess.run(loss_new, feed_dict={x1:xnp1, x2:xnp2, W_true:W_true_np})
+      sess.run(slow_op, feed_dict={x1: xnp1, x2:xnp2, W_true:W_true_np})
+      new_loss_slow = sess.run(loss_new, feed_dict={x1:xnp1, x2:xnp2, W_true:W_true_np})
 
-    # sess.run(slow_op, feed_dict={x: xnp2, W_true:W_true_np})
-    # print sess.run(y_eps)
     if (sample_w+1) % visualize_step == 0:
-      # print sess.run(grad_fast, feed_dict={x: xnp, W_true:W_true_np})
-      # sess.run(fast_op, feed_dict={x: xnp1, W_true:W_true_np})
       if print_debug_flag:
         print 'fast var:'
         if old_update_flag:
-          print sess.run(Wf, feed_dict={x:xnp1})
+          print sess.run(Wf, feed_dict={x1:xnp1})
         else:
-          print sess.run(Wf_new, feed_dict={x:xnp1, W_true:W_true_np})
+          print sess.run(Wf_new, feed_dict={x1:xnp1, W_true:W_true_np})
 
         print 'grad slow:'
         if old_update_flag:
           print sess.run(grad_slow, feed_dict={x:xnp2, W_true:W_true_np})
         else:
           print sess.run(grad_slow, feed_dict={x:xnp1, x2:xnp2, W_true:W_true_np})
-      # loss1 = sess.run(loss_new, feed_dict={x:xnp1, x2:xnp2, W_true:W_true_np})
-      # print 'slow grad new', sess.run(grads_Ws, feed_dict={x:xnp1, x2:xnp2, W_true:W_true_np})
-      # print 'slow grad old', sess.run(grad_slow_old, feed_dict={})
-      # sess.run(slow_op, feed_dict={x: xnp1, x2:xnp2, W_true:W_true_np})
-      # loss2 = sess.run(loss_new, feed_dict={x:xnp1, x2:xnp2, W_true:W_true_np})
-      # print 'Wf', sess.run(Wf)
-      #print 'Wf new: ', sess.run(Wf_new, feed_dict={x:xnp1, W_true:W_true_np})
 
       log.info('fast learner: old / new loss: %f, new loss: %f' % (old_loss, new_loss) )
       log.info('slow learner: old / new loss %f, new loss %f' % (old_loss_slow, new_loss_slow))
-      # log.info('Sample a new problem by sampling from W')
-      # print W_true_np
-      # log.info('step %d, loss: %f, %f' % (i+1, raw_loss, sess.run(loss, feed_dict={x:xnp2, W_true:W_true_np})))
-      # print 'slow learner', sess.run(Ws)
-      # print 'fast learner', sess.run(Wf)
-      # print 'combined', sess.run(W_pred)
       Ws_np = sess.run(Ws)
       print 'Current slow learner with determinant %f: ' % np.linalg.det(Ws_np)
       print Ws_np
 
-      print 'Ws^T * Ws', np.matmul(np.transpose(Ws_np), Ws_np)
-      print 'True Sigma inv', True_sigma_inv
+      print 'Ws^T * Ws'
+      print np.matmul(np.transpose(Ws_np), Ws_np)
+      print 'True Sigma inv / eta (theoretical bound):'
+      print True_sigma_inv / lr_fast
     
 # print 'In comparison with the slow learner, the true whitening:', np_transform_inv
 sess.close()
