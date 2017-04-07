9f4e64665a90bf2057a7ca1f1ee5d66715108647
zhuoyuan <chenzhuoyuan07@gmail.com>
2017-04-06 17:53:54 -0700

    while

--- while_test/main.py
+++ while_test/main.py
@@ -3,17 +3,36 @@ import glog as log
 
 
 if __name__ == '__main__':
-  i = tf.constant(0)
+  # example 1:
+  '''# i = tf.constant(0)
+  i = 0
+  
   # c: condition
-  c = lambda i: tf.less(i, 10)
+  c = lambda i: i<10# tf.less(i, 10)
+  
   # b: body
-  b = lambda i: tf.add(i,1)
+  def body(i):
+    return tf.add(i,1)
+  b = body # lambda i: tf.add(i,1)
+  
   # [i]: loop_vars
   r = tf.while_loop(c, b, [i])
+  
   # return type: r is a tensor, b/c tf.add(i,1) returns a tensor
-  log.info('r type is %s' % type(r))
+  log.info('r type is %s' % type(r))'''
 
-  print i # now zero
+  # example 2
+  unroll_len = 10
+  fx_array = tf.TensorArray(tf.int32, size=unroll_len)
+  c = lambda i, _: i<unroll_len
+  def time_step(i, fx_array):
+    fx_array = fx_array.write(i, i)
+    i = i+1
+    return i, fx_array
+    
+  _, fx_array = tf.while_loop(cond=c, body=time_step, loop_vars=[0, fx_array])
+  # print i # now zero
+  r = fx_array.stack()
   
   sess = tf.Session()
   # sess.run(tf.global_variables_initializer())
