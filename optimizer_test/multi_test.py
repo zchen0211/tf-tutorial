7c6bb2ee5858e6a781dfa0fdea9d1f0a1b0955a1
zchen0211 <chenzhuoyuan07@gmail.com>
2017-10-04 17:27:46 -0700

    simple rnn checked in

--- optimizer_test/multi_test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-
-tmp_y_np = np.ones((1, 10)).astype(np.float32)
-tmp_y = tf.get_variable('tmp_y', initializer=tmp_y_np)
-tmp_logit = tf.get_variable('tmp_logit', initializer=tmp_y_np)
